c220ca5b49a676ee9c7db7cea281f84096a8b3ee
radek <radekwrooobel02@gmail.com>
2024-04-26 11:07:34 +0200

    Trójkąt z pustym środkiem

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int rozmiar = 0;
+    cout << "Podaj rozmiar trojkata: \n";
+    cin >> rozmiar;
+
+    for (int i = 0; i < rozmiar; i++)
+    {
+        for (int j = 0; j < rozmiar; j++)
+        {
+            if (j == (rozmiar-1) || j == (rozmiar - 1) - i || i == (rozmiar-1))
+            {
+                cout << ("* ");
+            }
+            else
+            {
+                cout << (" ");
+            }
+        }
+        cout << endl;
+    }
+
+
+    return 0;
+}
